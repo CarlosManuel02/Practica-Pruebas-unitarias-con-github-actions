091608c467c4e8e6683cb24a0d2eb5005a837f0b
Carlos Manuel Cedeño Barrera <carlosmcb02@gmail.com>
2023-11-19 10:26:26 -0400

    error enn los tests

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
 app.get('/', (req, res) => {
     res.send('Hello World!, Carlos Manuel Cedeño Barrera 2022-0018');
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
-module.exports = app;
+module.exports = { app, server };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,14 @@
+// tests/index.test.js
 const request = require('supertest');
-const app = require('../src/index');
-
+const { app, server } = require('../src/index');
 
 describe('Test the root path', () => {
-  test('It should respond with "Hello World!"', async () => {
-      const response = await request(app).get('/');
-      expect(response.text).toBe('Hello World!, Carlos Manuel Cedeño Barrera 2022-0018');
-  });
+    test('It should respond with "Hello World!"', async () => {
+        const response = await request(app).get('/');
+        expect(response.text).toBe('Hello World!, Carlos Manuel Cedeño Barrera 2022-0018');
+    });
+});
+
+afterAll(() => {
+    server.close();
 });
